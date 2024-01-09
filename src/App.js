379e833ab6da2191a6c6c75c539beecccfb65f09
misterraina <ayushraina617@gmail.com>
2024-01-09 11:56:34 +0530

    Inital Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Header from "./components/header";
+import Footer from "./components/footer";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+
+const year = new Date().getFullYear();
+
+function Footer(){
+return(
+    <footer>
+        <p>copyright {year}</p>
+    </footer>
+)
+}
+
+export default Footer;
