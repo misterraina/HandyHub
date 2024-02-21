11504cfeb77670431afe97a13155125e447ec576
misterraina <ayushraina617@gmail.com>
2024-02-22 02:47:38 +0530

    topbar

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,15 +1,37 @@
 import "./topBar.css"
+import imgCenter from "../../images/logo_Center.png"
 
 export default function TopBar() {
   return (
     <div className="top">
-      <div className="topRight">
-        Right
-        </div>
-      <div className="topCenter">center</div>
-      <div className="topLeft">
-      Left
+
+<div className="topLeft">
+
+        <ul className="topList">
+          <li className="topListItems">Find Job</li>
+          <li className="topListItems">Post Job</li>
+          <li className="topListItems">Find Freelancer</li>
+          <li className="topListItems">Help Support</li>
+          <li className="topListItems">Login</li>
+        </ul>
+
       </div>
+
+
+      <div className="topCenter">
+        
+        <img className="topLogo" src={imgCenter} alt="home img" />
+
+        </div>
+
+
+     
+
+      <div className="topRight">
+        <span className="topLogin">Login</span>
+
+        </div>
+
     </div>
   )
 }
